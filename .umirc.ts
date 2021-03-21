99bc213b8757d28c940adbea2cf60f3c728fd193
ibaiwei <bawei3600@gmail.com>
2021-03-22 01:38:04 +0800

    wip(dva): dva

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'umi';
 import routes from './src/config/routes';
 
 export default defineConfig({
+  title: 'My Umi Base',
   publicPath: '/',
   history: {
     type: 'browser',
