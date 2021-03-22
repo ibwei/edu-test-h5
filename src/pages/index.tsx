cc56859c058c3c4643b29244843cc41d05cc10a8
ibaiwei <bawei3600@gmail.com>
2021-03-22 17:56:59 +0800

    style(index): 首页调整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ export default function IndexPage(props: any) {
   return (
     <div className="app-container">
       <h1 className="title">Page index</h1>
-      <Link to="/me">me Page</Link>
-      <Link to="/test">test Page</Link>
-      <Link to="/test/index">test index Page</Link>
-      <Link to="/test/test2">test test2 Page</Link>
-      <div>test2</div>
+      <Link to="/me">Me Page</Link>
+      <Link to="/test">测试 layout</Link>
+      <Link to="/test/index">测试 Dva</Link>
+      <Link to="/test/test2">子路由测试</Link>
+      <Link to="/tes22t/te33st2/32323">404测试</Link>
     </div>
   );
 }
