6af32a3ea4aa181cbd80f7c0432157d8d2a9b4a5
ibaiwei <bawei3600@gmail.com>
2021-03-22 17:27:46 +0800

    feat(dva): 初步完善好 dva 部分

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,7 +1,29 @@
-export default function IndexPage() {
+import { connect } from 'dva';
+function IndexPage(props: any) {
+  const { add, minus, init, count } = props;
   return (
     <div>
-      <h1>Me index</h1>
+      <button onClick={() => add(1)}>增加 1</button>
+      <button onClick={() => minus(1)}>减少 1</button>
+      <button onClick={() => init()}>init</button>
+      state={count}
     </div>
   );
 }
+
+const mapStatetoprops = (state: any) => ({
+  count: state.count,
+});
+
+const actionCreater = {
+  add: (payload: number) => ({ type: 'count/add', payload }),
+  minus: (payload: number) => ({ type: 'count/minus', payload }),
+  init: (payload: number) => {
+    return {
+      type: 'count/init',
+      payload,
+    };
+  },
+};
+
+export default connect(mapStatetoprops, actionCreater)(IndexPage);
