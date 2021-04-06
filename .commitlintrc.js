bdee796846e0847125dd1bd2dfd069f26a9a5457
ibaiwei <bawei3600@gmail.com>
2021-04-06 21:59:18 +0800

    feat(husky): add husky

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,30 +3,4 @@
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'upd',
-        'feat',
-        'fix',
-        'refactor',
-        'docs',
-        'chore',
-        'style',
-        'revert',
-        'wip',
-        'ci',
-        'perf',
-      ],
-    ],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72],
-  },
 };
